406528f69b84de360a8bf08a4813612601015a3e
Jonathan Coulthard <Jon@jonathans-mbp-4.home>
2019-12-07 18:49:22 +0000

    Day 7 part 2

--- Day7.c
+++ Day7.c
@@ -73,6 +73,7 @@ typedef struct {
 	i32 *memory;
 	i32 cursor;
 	bool running;
+	bool waiting;
 	i32 *inputQueue;
 	i32 inputCursor;
 	i32 *outputQueue;
@@ -80,7 +81,7 @@ typedef struct {
 
 void compute(computer *c) {
 
-	while (c->running) {
+	while (c->running && !c->waiting) {
 		i32 opcode = c->memory[c->cursor] % 100;
 		i32 mode1 = (c->memory[c->cursor] / 100)%10;
 		i32 mode2 = (c->memory[c->cursor] / 1000)%10;
@@ -119,8 +120,15 @@ void compute(computer *c) {
 					return;
 				}
 				i32 write = c->memory[c->cursor+1];
-				c->memory[write] = c->inputQueue[c->inputCursor++];
-				c->cursor += 2;
+				if (c->inputCursor < buf_len(c->inputQueue)) {
+					c->memory[write] = c->inputQueue[c->inputCursor++];
+					c->cursor += 2;
+				} else {
+					// if we don't have any more inputs in the queue,
+					// go to wait mode.  
+					c->waiting = true;
+					return;
+				}
 			} break;
 			case 4: { // output
 				i32 read = c->memory[c->cursor+1];
@@ -189,27 +197,58 @@ i32 *buf_copy(i32 *input) {
 	return copy;
 }
 
-bool 
+bool anyComputersRunning(computer *computers, i32 numComputers) {
+	for (i32 i = 0; i < numComputers; i++) {
+		if (computers[i].running) {
+			return true;
+		} 
+	}
+	return false;
+}
 
 i32 runAmplifierCircuit(i32 *program, i32 *phaseSettings, i32 numAmps) {
-	i32 currInput = 0;
+	computer C[5]; 
+
+	// initialise the computers
 	for (i32 i = 0; i < numAmps; i++) {
 		computer A = {};
 		A.memory = buf_copy(program);
-		buf_push(A.inputQueue, phaseSettings[i]);
-		buf_push(A.inputQueue, currInput);
 		A.running = true;
+		A.waiting = true;
+		buf_push(A.inputQueue, phaseSettings[i]);
 
-		compute(&A);
-
-		currInput = A.outputQueue[0];
-		buf_free(A.memory);
-		buf_free(A.inputQueue);
-		buf_free(A.outputQueue);
+		C[i] = A;
 	}
 
-	i32 output = currInput;
-	return output;
+	i32 i = 0;
+	i32 nextInput = 0;
+	while (anyComputersRunning(C, numAmps)) {
+		// printf("Amplifier %d running \n", i);
+		if (C[i].waiting) {
+			buf_push(C[i].inputQueue, nextInput);
+			C[i].waiting = false;
+		} else {
+			printf("The next computer was not waiting for an input. Returning.\n");
+			return nextInput;
+		}
+		
+		compute(&C[i]);
+		// at this point C[i] has either halted or is waiting for another input
+		i32 lastOutputIndex = buf_len(C[i].outputQueue)-1; 
+		nextInput = C[i].outputQueue[lastOutputIndex]; 
+		// printf("next input = %d\n", nextInput);
+
+		i = (i+1)%numAmps;
+	}
+
+	// free all of the computers
+	for (i32 i = 0; i < numAmps; i++) {
+		buf_free(C[i].inputQueue);
+		buf_free(C[i].outputQueue);
+		buf_free(C[i].memory);
+	}
+
+	return nextInput;
 } 
 
 void swap(i32 *a, i32 *b) {
@@ -253,9 +292,7 @@ void printPermuations(i32 *array) {
 	printf("num permutations found: %d\n", numPermutations);
 }
 
-i32 optimisePhaseSettings(i32 *program) {
-	i32 numAmps = 5;
-	i32 phaseSettings[] = {0,1,2,3,4};
+i32 optimisePhaseSettings(i32 *program, i32 *phaseSettings, i32 numAmps) {
 	i32 maxOutput = runAmplifierCircuit(program, phaseSettings, numAmps);
 		
 	i32 c[] = {0,0,0,0,0};
@@ -268,7 +305,6 @@ i32 optimisePhaseSettings(i32 *program) {
 			}
 		}
 	}
-	
 	return maxOutput;
 }
 
@@ -277,30 +313,52 @@ void test() {
 	i32 phaseSettings[] = {4,3,2,1,0};
 	runAmplifierCircuit(program1, phaseSettings, 5);
 
-	i32 maxOutput1 = optimisePhaseSettings(program1);
+	i32 maxOutput1 = optimisePhaseSettings(program1, phaseSettings, 5);
 	printf("max output for program 1: %d\n", maxOutput1);
 
 	i32 *program2 = parseArray("Day7_test2.txt");
 	i32 phaseSettings2[] = {0,1,2,3,4};
 	runAmplifierCircuit(program2, phaseSettings2, 5);
 
-	i32 maxOutput2 = optimisePhaseSettings(program2);
+	i32 maxOutput2 = optimisePhaseSettings(program2, phaseSettings, 5);
 	printf("max output for program 2: %d\n", maxOutput2);
 
 	i32 *program3 = parseArray("Day7_test3.txt");
 	i32 phaseSettings3[] = {1,0,4,3,2};
 	runAmplifierCircuit(program3, phaseSettings3, 5);
 
-	i32 maxOutput3 = optimisePhaseSettings(program3);
-	printf("max output for program 2: %d\n", maxOutput3);
+	i32 maxOutput3 = optimisePhaseSettings(program3, phaseSettings, 5);
+	printf("max output for program 3: %d\n", maxOutput3);
+}
+
+void testPart2() {
+	i32 *program4 = parseArray("Day7_test4.txt");
+	i32 phaseSettings[] = {9,8,7,6,5};
+	i32 output = runAmplifierCircuit(program4, phaseSettings, 5);
+	printf("output for program4 is %d\n", output);
+
+	i32 maxOutput4 = optimisePhaseSettings(program4, phaseSettings, 5);
+	printf("max output for program 4: %d\n", maxOutput4);
+
+	i32 *program5 = parseArray("Day7_test5.txt");
+	i32 phaseSettings5[] = {9,7,8,5,6};
+	i32 output5 = runAmplifierCircuit(program5, phaseSettings5, 5);
+	printf("output for program5 is %d\n", output5);
+
+	i32 maxOutput5 = optimisePhaseSettings(program5, phaseSettings, 5);
+	printf("max output for program 5: %d\n", maxOutput5);
 }
 
 int main(int argc, char const *argv[]) {
 	test();
-
 	i32 *program = parseArray("Day7_input.txt");
-	i32 maxOutput = optimisePhaseSettings(program);
+	i32 phaseSettings[] = {0,1,2,3,4};
+	i32 maxOutput = optimisePhaseSettings(program, phaseSettings, 5);
 	printf("Part 1: %d\n", maxOutput);
 
+	testPart2();
+	i32 phaseSettings2[] = {5,6,7,8,9};
+	i32 maxOutput2 = optimisePhaseSettings(program, phaseSettings2, 5);
+	printf("Part 2: %d\n", maxOutput2);
 	return 0;
 }
